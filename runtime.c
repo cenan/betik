b36e61121143c7608572eccf2eb4d19355ae24b5
cenanozen <cenan.ozen@gmail.com>
2011-06-16 21:23:13 +0300

    added missing runtime files

---
+++ runtime.c
@@ -0,0 +1,103 @@
+/*
+ Betik
+ Copyright (C) 2010-2011 cenan ozen <cenan.ozen[at]gmail.com>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "runtime.h"
+
+scope_t* create_scope()
+{
+	scope_t* scope = (scope_t*)malloc(sizeof(scope_t));
+	scope->variables = create_list();
+	return scope;
+}
+
+void destroy_scope(scope_t* s)
+{
+	destroy_list(s->variables);
+	free(s);
+}
+
+variable_t* get_variable(runtime_t* rt, char* variable_name)
+{
+	int i;
+	for (i = 0; i < list_get_item_count(rt->current_scope->variables); i++) {
+		variable_t* var = list_get_item(rt->current_scope->variables, i);
+		if (strcmp(variable_name, var->name) == 0) {
+			return var;
+		}
+	}
+	for (i = 0; i < list_get_item_count(rt->global_scope->variables); i++) {
+		variable_t* var = list_get_item(rt->global_scope->variables, i);
+		if (strcmp(variable_name, var->name) == 0) {
+			return var;
+		}
+	}
+	return 0;
+}
+
+variable_t* create_variable(runtime_t* rt, char* variable_name)
+{
+	variable_t* var = (variable_t*)malloc(sizeof(variable_t));
+	var->name = variable_name;
+	var->obj = 0;
+	list_insert(rt->current_scope->variables, var);
+
+	return var;
+}
+
+object_t* create_object(object_type_t obj_type)
+{
+	object_t* obj = (object_t*)malloc(sizeof(object_t));
+
+	obj->type = obj_type;
+	obj->reference_count = 0;
+
+	return obj;
+}
+
+variable_t* call_variable_op(runtime_t* rt, variable_t* var1, variable_t* var2, token_type_t tok)
+{
+	variable_t* var = create_variable(rt, "#");
+	var->obj = create_object(OBJ_NUMBER);
+	var->obj->reference_count += 1;
+	if (TT_OP_ADD == tok) {
+		var->obj->data = (void*)((int)var1->obj->data + (int)var2->obj->data);
+	} else if (TT_OP_SUB == tok) {
+		var->obj->data = (void*)((int)var1->obj->data - (int)var2->obj->data);
+	} else if (TT_OP_MUL == tok) {
+		var->obj->data = (void*)((int)var1->obj->data * (int)var2->obj->data);
+	} else if (TT_OP_DIV == tok) {
+		if (0 == (int)var2->obj->data) {
+			fprintf(stderr, "Division by zero\n");
+			exit(EXIT_FAILURE);
+		}
+		var->obj->data = (void*)((int)var1->obj->data / (int)var2->obj->data);
+	} else if (TT_OP_GT == tok) {
+		var->obj->data = (void*)((int)var1->obj->data > (int)var2->obj->data);
+	} else if (TT_OP_LT == tok) {
+		var->obj->data = (void*)((int)var1->obj->data < (int)var2->obj->data);
+	} else if (TT_OP_EQUAL == tok) {
+		var1->obj = var2->obj;
+		var2->obj->reference_count += 1;
+	}
+	return var;
+}
+
---
+++ runtime.h
@@ -0,0 +1,63 @@
+/*
+ Betik
+ Copyright (C) 2010-2011 cenan ozen <cenan.ozen[at]gmail.com>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef runtime_h
+#define runtime_h
+
+#include "common.h"
+#include "parser.h"
+
+
+typedef enum {
+	OBJ_BASE,
+	OBJ_NUMBER,
+	OBJ_STRING,
+	OBJ_FUNCTION,
+} object_type_t;
+
+typedef struct {
+	list_t* variables;
+} scope_t;
+
+typedef struct {
+	object_type_t type;
+	int reference_count;
+	void* data;
+} object_t;
+
+typedef struct {
+	char* name;
+	object_t* obj;
+} variable_t;
+
+typedef struct {
+	stack_t* scopes;
+	scope_t* global_scope;
+	scope_t* current_scope;
+	ast_t* ast;
+} runtime_t;
+
+scope_t* create_scope();
+void destroy_scope(scope_t* s);
+variable_t* get_variable(runtime_t* rt, char* variable_name);
+variable_t* create_variable(runtime_t* rt, char* variable_name);
+object_t* create_object(object_type_t obj_type);
+variable_t* call_variable_op(runtime_t* rt, variable_t* var1, variable_t* var2, token_type_t tok);
+
+#endif // runtime_h
+
