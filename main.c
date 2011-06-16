833f4a46c94fa5413f060de5eab30b63375c9c18
cenanozen <cenan.ozen@gmail.com>
2011-06-16 08:05:47 +0300

    fixed header

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*
  Betik
- Copyright (C) 2010 cenan ozen <cenan.ozen[at]gmail.com>
+ Copyright (C) 2010-2011 cenan ozen <cenan.ozen[at]gmail.com>
 
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
