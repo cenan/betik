7760870386a25bb80b4e44426d36087977347b1a
cenan <me@cenanozen.com>
2013-06-07 13:54:34 +0300

    updated copyright headers

--- interpreter.h
+++ interpreter.h
@@ -1,5 +1,5 @@
 /**
- * Copyright 2010-2012 Cenan Ozen <cenan.ozen@gmail.com>
+ * Copyright 2010-2013 Cenan Ozen <cenan.ozen@gmail.com>
  * This file is part of Betik.
  *
  * Betik is free software: you can redistribute it and/or modify
