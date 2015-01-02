2dd46817675217f012fded588185d8de69a029c7
cenan <me@cenanozen.com>
2015-01-03 00:01:52 +0200

    updates doc tags

--- interpreter.c
+++ interpreter.c
@@ -1,5 +1,5 @@
 /**
- * Copyright 2010-2014 Cenan Ozen <betik@cenanozen.com>
+ * Copyright 2010-2015 Cenan Ozen <betik@cenanozen.com>
  * This file is part of Betik.
  *
  * Betik is free software: you can redistribute it and/or modify
